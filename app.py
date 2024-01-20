b96f54172b272f3b0efe104132f3fb12158f37b1
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-01-20 22:17:08 +0100

    Implements '/venues/<int:venue_id>'
    * implementes additonal TODO add delete button

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 import dateutil.parser
 import babel
 import logging
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, abort
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from logging import Formatter, FileHandler
@@ -153,14 +153,29 @@ def create_venue_submission():
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
   return render_template('pages/home.html')
 
-@app.route('/venues/<venue_id>', methods=['DELETE'])
+@app.route('/venues/<int:venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
   # TODO: Complete this endpoint for taking a venue_id, and using
   # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
+  error = False
+  try:
+    venue : Venue = Venue.query.get(venue_id)
+    db.session.delete(venue)
+    db.session.commit()
+    
+  except Exception as e:
+    db.session.rollback()
+    error = True
 
-  # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
-  # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+  finally:
+    db.session.close()
+
+  if error:
+        abort(500)
+  else:
+    # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
+    # clicking that button delete it from the db then redirect the user to the homepage
+    return render_template('pages/home.html')
 
 #  Artists
 #  ----------------------------------------------------------------
